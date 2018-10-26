e04c28906a55e5b208eb1c62bc52f2af4bcf064d
Jack McCallum <mccallumjack@gmail.com>
2018-10-26 16:23:24 +1100

    Handle OAuth requests

--- lib/cloudsight_elixir/api.ex
+++ lib/cloudsight_elixir/api.ex
@@ -5,22 +5,24 @@ defmodule CloudsightElixir.Api do
 
   use HTTPoison.Base
 
+  alias CloudsightElixir.{Client, OAuth1Client}
+
   @spec get(binary, Client.t) :: {atom, map}
   def get(path, client) do
     _request(:get, process_url(path, client), client)
   end
 
-  @spec post(binary, binary, Client.t) :: {atom, map}
-  def post(path, body, client) do
-    _request(:post, process_url(path, client), client, body)
+  @spec post(binary, map, Client.t) :: {atom, map}
+  def post(path, params, client) do
+    _request(:post, process_url(path, client), client, params)
   end
 
-  @spec _request(:get | :post, binary, Client.t, binary) :: {atom, map}
-  def _request(method, url, client, body \\ "") do
-    headers = header_for(body) ++ [{"Authorization", authorization_header(client)}]
+  @spec _request(:get | :post, binary, Client.t, map | {:multipart, map}) :: {atom, map}
+  def _request(method, url, client, params \\ %{}) do
+    headers = headers_for(method, url, client, params)
 
     method
-    |> raw_request(url, body, headers)
+    |> raw_request(url, encode_body(params), headers)
     |> handle_response
   end
 
@@ -39,17 +41,81 @@ defmodule CloudsightElixir.Api do
     client.endpoint <> path
   end
 
-  @spec authorization_header(Client.t) :: binary
-  def authorization_header(client) do
-    "CloudSight #{client.api_key}"
-  end
-
   def process_response_body(""),  do: ""
   def process_response_body(body) do
     body
     |> Poison.decode!
   end
 
-  def header_for({:multipart, _}), do: [{"Content-type", "application/x-www-form-urlencoded"}]
-  def header_for(_), do: [{"Content-type", "application/json"}]
+  def headers_for(method, url, %OAuth1Client{} = client, params) do
+    [
+      content_type_header(params),
+      oauth_signature(client, method, params, url)
+    ]
+  end
+
+  def headers_for(_method, _url, %Client{} = client, params) do
+    [
+      content_type_header(params),
+      basic_auth_header(client)
+    ]
+  end
+
+  defp content_type_header(%{image: _}), do: multipart_content_type_header()
+  defp content_type_header(%{video: _}), do: multipart_content_type_header()
+  defp content_type_header(_), do: json_content_type_header()
+
+  defp oauth_signature(client, method, params, url) do
+    OAuther.sign(
+      Atom.to_string(method),
+      url,
+      convert_oauth_params_for_signature(params),
+      client.credentials
+    ) |> OAuther.header |> elem(0)
+  end
+
+  defp basic_auth_header(client) do
+    {"Authorization", "CloudSight #{client.api_key}"}
+  end
+
+  defp json_content_type_header() do
+    {"content-type", "application/json"}
+  end
+
+  defp multipart_content_type_header() do
+    {"content-type", "application/x-www-form-urlencoded"}
+  end
+
+  defp convert_oauth_params_for_signature(params) do
+    params
+    |> convert_atom_map_to_string_map
+    |> Enum.reject(fn {k, _v} -> k == "image"|| k == "video" end)
+  end
+
+  defp convert_atom_map_to_string_map(keyword_map) do
+    for {key, val} <- keyword_map, into: %{}, do: {to_string(key), val}
+  end
+
+  @spec encode_body(map) :: binary | {:multipart, list}
+  defp encode_body(%{image: _image} = map), do: encode_multipart_body(map, :image)
+  defp encode_body(%{video: _video} = map), do: encode_multipart_body(map, :video)
+  defp encode_body(options) do
+    options
+    |> Poison.encode!
+  end
+
+  @spec encode_multipart_body(map, atom) :: {atom, list}
+  defp encode_multipart_body(map, file_type) do
+    {path, map} = Map.pop(map, file_type)
+
+    additional_params = Enum.reduce(map, [], fn {k, v}, acc ->
+      acc ++ [{to_string(k), v}]
+    end)
+
+    {:multipart,
+      [
+        {:file, path, {"form-data", [name: to_string(file_type), filename: Path.basename(path)]}, []}
+      ] ++ additional_params
+    }
+  end
 end
--- test/cloudsight_elixir/api_test.exs
+++ test/cloudsight_elixir/api_test.exs
@@ -13,8 +13,43 @@ defmodule CloudsightElixir.ApiTest do
     assert process_url("/test_path", client) == "api.lvh.me:3000/test_path"
   end
 
-  test "authorization header with api_key", %{client: client} do
-    assert authorization_header(client) == "CloudSight test_api_key"
+  test "headers for json with oauth" do
+    client = CloudsightElixir.OAuth1Client.new(%{consumer_key: "abcd", consumer_secret: "abcd"})
+    body = %{
+      locale: "en",
+      remote_image_url: "http://example.com/pic.jpg"
+    }
+
+    [content_type, auth] = headers_for(:post, "http://cloudsight.ai/v1/images", client, body)
+
+    assert content_type == {"content-type", "application/json"}
+    assert {"Authorization", "OAuth oauth_signature" <> _rest} = auth
   end
 
+  test "headers for multipart with oauth" do
+    client = CloudsightElixir.OAuth1Client.new(%{consumer_key: "abcd", consumer_secret: "abcd"})
+    body = %{
+      locale: "en",
+      image: "test/support/logo.png"
+    }
+
+    [content_type, auth] = headers_for(:post, "http://cloudsight.ai/v1/images", client, body)
+
+    assert content_type == {"content-type", "application/x-www-form-urlencoded"}
+    assert {"Authorization", "OAuth oauth_signature" <> _rest} = auth
+  end
+
+  test "headers for json with basic auth", %{client: client} do
+    [content_type, auth] = headers_for(:post, "http://cloudsight.ai/v1/images", client, %{locale: "en"})
+
+    assert content_type == {"content-type", "application/json"}
+    assert auth == {"Authorization", "CloudSight test_api_key"}
+  end
+
+  test "headers for multipart with basic auth", %{client: client} do
+    [content_type, auth] = headers_for(:post, "http://cloudsight.ai/v1/images", client, %{image: "an_image.jpg"})
+
+    assert content_type == {"content-type", "application/x-www-form-urlencoded"}
+    assert auth == {"Authorization", "CloudSight test_api_key"}
+  end
 end
